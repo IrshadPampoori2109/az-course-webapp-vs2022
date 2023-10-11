f117a7b45f76aeb5c732f1dc1bd5da69e98d087b
Irshad Pampoori <Irshad.Pampoori@rsystems.com>
2023-10-11 11:31:15 +0530

    Add project files.

---
+++ az-course-webapp-vs2022/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace az_course_webapp_vs2022.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
